cd3b9083a322d06a8f91636fe45f8731e1287336
hxrkkd <66399435+hxrkkd@users.noreply.github.com>
2020-06-09 12:42:40 +0800

    Update restaurantview.php

--- restaurantview.php
+++ restaurantview.php
@@ -34,22 +34,22 @@ $connectionOptions = array(
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 if (!$conn)
 {
-die("Error connection: ".sqlsrv_errors());
+  die("Error connection: ".sqlsrv_errors());
 }
 $tsql= "SELECT * FROM [dbo].[restaurant]";
 $getResults= sqlsrv_query($conn, $tsql);
 if ($getResults == FALSE)
 {
-die(sqlsrv_errors());
+  die(sqlsrv_errors());
 }
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
 {
-echo "<tr>";
-echo "<td>". $row['restaurant_id'] . "</td>";
-echo "<td>". $row['restaurant_name'] ."</td>";
-echo "<td>". $row['restaurant_address'] . "</td>";
-echo "<td>". $row['restaurant_phone'] . "</td>";
-echo "</tr>";
+   echo "<tr>";
+   echo "<td>". $row['restaurant_id'] . "</td>";
+   echo "<td>". $row['restaurant_name'] ."</td>";
+   echo "<td>". $row['restaurant_address'] . "</td>";
+   echo "<td>". $row['restaurant_phone'] . "</td>";
+   echo "</tr>";
 }
 sqlsrv_free_stmt($getResults);
 ?>
