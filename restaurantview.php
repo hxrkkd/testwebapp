58f65094d06a8e12b9aeac8d063e1149e61d0a66
hxrkkd <66399435+hxrkkd@users.noreply.github.com>
2020-06-09 12:30:02 +0800

    Create restaurantview.php

---
+++ restaurantview.php
@@ -0,0 +1,29 @@
+<?php
+$serverName = "testphptp041964.database.windows.net";
+$connectionOptions = array(
+"Database" => "testPhpTp041964",
+"Uid" => "Tp041964",
+"PWD" => "Apu123456");
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if (!$conn)
+{
+die("Error connection: ".sqlsrv_errors());
+}
+$tsql= "SELECT * FROM [dbo].[restaurant]";
+$getResults= sqlsrv_query($conn, $tsql);
+if ($getResults == FALSE)
+{
+die(sqlsrv_errors());
+}
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+{
+echo "<tr>";
+echo "<td>". $row['restaurant_id'] . "</td>";
+echo "<td>". $row['restaurant_name'] ."</td>";
+echo "<td>". $row['restaurant_address'] . "</td>";
+echo "<td>". $row['restaurant_phone'] . "</td>";
+echo "</tr>";
+}
+sqlsrv_free_stmt($getResults);
+?>
