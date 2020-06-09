2fc6245f6fc2c7e71f417809cfca1a10983ce68b
hxrkkd <66399435+hxrkkd@users.noreply.github.com>
2020-06-09 12:40:15 +0800

    Update restaurantview.php

--- restaurantview.php
+++ restaurantview.php
@@ -1,3 +1,29 @@
+<html>
+<head>
+<title>List of KL Restaurant</title>
+</head>
+<body>
+<center>
+<h1>List of KL Restaurant</h1>
+<!--create table structure using HTML first-->
+<table border="1">
+<tr>
+<th>Restaurant ID</th>
+<th>Restaurant Name</th>
+<th>Address</th>
+<th>Phone</th>
+</tr>
+<tr>
+<th>0</th>
+<th>Subway Restaurant</th>
+<th>Menara Standard Chartered, TPM</th>
+<th>03-22441234</th>
+</tr>
+</table>
+</center>
+</body>
+</html>
+
 <?php
 $serverName = "testphptp041964.database.windows.net";
 $connectionOptions = array(
@@ -28,28 +54,4 @@ echo "</tr>";
 sqlsrv_free_stmt($getResults);
 ?>
 
-<html>
-<head>
-<title>List of KL Restaurant</title>
-</head>
-<body>
-<center>
-<h1>List of KL Restaurant</h1>
-<!--create table structure using HTML first-->
-<table border="1">
-<tr>
-<th>Restaurant ID</th>
-<th>Restaurant Name</th>
-<th>Address</th>
-<th>Phone</th>
-</tr>
-<tr>
-<th>0</th>
-<th>Subway Restaurant</th>
-<th>Menara Standard Chartered, TPM</th>
-<th>03-22441234</th>
-</tr>
-</table>
-</center>
-</body>
-</html>
+
